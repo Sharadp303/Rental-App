43048b8f74f67279d5c585601e8c7f173ebb17a3
Sharad <sharad.patels03@gmail.com>
2025-07-10 13:11:09 +0530

    Client setUp

---
+++ client/src/state/redux.tsx
@@ -0,0 +1,49 @@
+import { combineReducers, configureStore } from "@reduxjs/toolkit";
+import globalReducer from "@/state";
+import { api } from "@/state/api";
+import {
+  Provider,
+  TypedUseSelectorHook,
+  useDispatch,
+  useSelector,
+} from "react-redux";
+import { useRef } from "react";
+import { setupListeners } from "@reduxjs/toolkit/query";
+
+// Redux Store
+const rootReducer = combineReducers({
+  global: globalReducer,
+  [api.reducerPath]: api.reducer,
+});
+
+export const makeStore = () => {
+  return configureStore({
+    reducer: rootReducer,
+    middleware: (getDefaultMiddleware) =>
+      getDefaultMiddleware().concat(api.middleware),
+  });
+};
+
+// Redux Types
+
+export type AppStore = ReturnType<typeof makeStore>;
+export type RootState = ReturnType<AppStore["getState"]>;
+export type AppDispatch = AppStore["dispatch"];
+export const useAppDispatch = () => useDispatch<AppDispatch>();
+export const useAppSelector: TypedUseSelectorHook<RootState> = useSelector;
+
+// Provider
+
+export default function StoreProvider({
+  children,
+}: {
+  children: React.ReactNode;
+}) {
+  const storeRef = useRef<AppStore | null>(null);
+  if (!storeRef.current) {
+    storeRef.current = makeStore();
+    setupListeners(storeRef.current.dispatch);
+  }
+
+  return <Provider store={storeRef.current}>{children}</Provider>;
+}
